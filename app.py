b2520de7d679cc5d44fd5b86633431e7e67989ae
obiomaokorowu <austinobi983@gmail.com>
2025-05-04 17:55:11 +0100

    updated code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DockerHub - Realcloud EC2 on Ubuntu 24.04!"
+    return "Hello from DockerHub -Realcloud EC2 on Ubuntu 24.04!, I hope you are enjoing this class"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
